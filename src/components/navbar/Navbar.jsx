e9ea302abbf0eee33dbcb1836676650f2b2248ff
Avishka Chamod <avishkachamod14@gmail.com>
2025-05-07 13:49:01 +0530

    Second

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,67 @@
+import "./Navbar.css";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import ShoppingBagOutlinedIcon from "@mui/icons-material/ShoppingBagOutlined";
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="navbar-right">
+          <img src="src\assets\icons\chronolux-png.png" className="logo-nav" />
+          <span className="nav-title">ChronoLux</span>
+        </div>
+
+        <div className="navbar-center">
+          <ul>
+            <li>HOME</li>
+            <li>SHOP</li>
+            <li>BRANDS</li>
+            <li>ACCESSORIES</li>
+            <li>OFFERS</li>
+          </ul>
+        </div>
+
+        <div className="navbar-left">
+          <div className="search-box-icon">
+            <SearchIcon className="search-icon-nav" />
+          </div>
+
+          <div className="fav-box-icon">
+            <FavoriteBorderOutlinedIcon className="fav-icon" />
+            <span className="fav-icon-amount">1</span>
+          </div>
+
+          <div className="shopping-icon">
+            <ShoppingBagOutlinedIcon className="shop-icon" />
+            <span className="shopping-icon-amount">3</span>
+          </div>
+
+          <div className="account-icon">
+            <AccountCircleOutlinedIcon className="account-icon-m" />
+          </div>
+        </div>
+      </nav>
+       
+       <nav className="nav-small">
+          
+          <div className="small-nav-heading">
+                <p>Welcome to ChronoLux ! Your Ultimate Luxury Watch Partnter !</p>
+          </div>
+
+          <div className="small-nav-end"> 
+            <ul>
+              <li>About Us</li>
+              <li>Contact</li>
+              <li>Join Us</li>
+            </ul>
+
+          </div>
+
+       </nav>
+
+
+    </div>
+  );
+}
