d1b02628a6de91b3377aa5a958c64ff8e5b06912
Avishka Chamod <avishkachamod14@gmail.com>
2025-05-10 21:10:30 +0530

    10/05-4

--- src/components/home_components/Brands.jsx
+++ src/components/home_components/Brands.jsx
@@ -13,24 +13,48 @@ export default function Brands() {
             <li>Rolex</li>
             <li>Patek Philippe</li>
             <li>Audemars Piguet</li>
-            <li>Omega</li>
-            <li>Richard Mille</li>
             <li>Hublot</li>
+            <li>Richard Mille</li>
+            <li>Omega</li>
             <li>Breitling</li>
             <li>Panerai</li>
             <li>Jaeger-LeCoultre</li>
             <li>Breguet</li>
+            <li className="brand-h1"> & More !</li>
           </ul>
-          <h1 className="brand-h1">+ More !</h1>
         </div>
 
         <div className="brand-watch-photos-container">
             
             <div className="watch-p-c">
-                
-
+                <img src="src\assets\home-img\rolex.jpg"  />
             </div>
 
+            <div className="watch-p-c">
+                <img src="src\assets\home-img\patek-philippe.jpg"  />
+            </div>
+
+            <div className="watch-p-c">
+                <img src="src\assets\home-img\audemars.jpg"  />
+            </div>
+
+            <div className="watch-p-c">
+                <img src="src\assets\home-img\hublot.jpg"  />
+            </div>
+   
+        </div>
+
+        <div className="brands-heading-second">
+          <h1 className="h1-second">Start Shopping Now!</h1>
+          <div className="sign-in-container">
+          <h3 className="h3-second">Log In or Create a Account to Get Started!</h3>
+          <a href="" className="brands-btn-log-in">Log In</a>
+          <a href="" className="brands-btn-sign-in">Create Account</a>
+          </div>
+        </div>
+
+        <div style={{ width: "90vw", textAlign: "center", padding:"15px" }}>
+          <hr />
         </div>
 
       </div>
