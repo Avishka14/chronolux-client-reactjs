6aa8c189226524a59a30f6f2857a6f9a21e72314
Avishka Chamod <avishkachamod14@gmail.com>
2025-05-28 15:35:28 +0530

    accessories page completed

--- src/components/home_routes/route_components/Accessories_Route/Accessories_Grid.jsx
+++ src/components/home_routes/route_components/Accessories_Route/Accessories_Grid.jsx
@@ -5,19 +5,19 @@ export default function Accessories_Grid({ brand, children }) {
   return (
     <div>
       <div className="heading-div">{children}</div>
-      <div className="mt-6 grid grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10 product-list-latest">
+      <div className="mt-6 grid grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10 img-cont-ac">
         {brand.accessorie.map((accessorie) => (
-          <div key={accessorie.id} className="group relative">
+          <div key={accessorie.id} className="group relative ">
             <img
               src={accessorie.imageSrc}
-              alt={accessorie.name}
+              alt={brand.name}
               className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75"
             />
             <div className="mt-4 flex justify-between">
               <div>
                 <h3 className="text-sm">
                 </h3>
-                <p className="mt-1 text-sm text-gray-500">{brand.name}</p>
+                <p className="mt-1 text-sm text-gray-500">{accessorie.name}</p>
               </div>
             </div>
           </div>
