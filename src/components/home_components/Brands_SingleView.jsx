5e9cd59bb0c04bc485432d80009152174feaa631
Avishka Chamod <avishkachamod14@gmail.com>
2025-06-07 20:50:06 +0530

    commit 07/06

--- src/components/home_components/Brands_SingleView.jsx
+++ src/components/home_components/Brands_SingleView.jsx
@@ -1,17 +1,38 @@
-import React from 'react';
-import { brands } from './brands-data/Brands_Data';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import "./HomeComponents.css";
+import { brands } from "./brands-data/Brands_Data";
+import { useParams } from "react-router-dom";
+import NavBar from "../../components/navbar/Navbar";
+import Footer from "../footer/Footer";
 
 function Brands_SingleView() {
-  const {id} = useParams();
+  const { id } = useParams();
   const brand = brands[id];
 
-  if (!brand) return <p> Brand not Available</p>
+  if (!brand) return <p> Brand not Available</p>;
   return (
-    <div>
-      <h2>{brand.brandName}</h2>
-    </div>
-  )
+    <>
+      <NavBar />
+      
+      <div className="brands-singleview-container">
+        <div className="coverPage-container">
+          <div className="singleView-img-container">
+            <h1 className="singleView-heading"> </h1>
+          </div>
+        </div>
+
+        <div className="singleView-section-II">
+          <div className="brand-name-heading">
+            <h1>
+              Shop Your Favourite Watches from {brand.brandName} with ChronoLux!
+            </h1>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
+    </>
+  );
 }
 
-export default Brands_SingleView
+export default Brands_SingleView;
